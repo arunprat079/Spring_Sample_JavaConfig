7a0d0bece957df8793f662b47363c4ce2f7baf40
arpratap <arpratap@DIN60015987.corp.capgemini.com>
2019-07-08 18:29:20 +0530

    Name Change

--- src/org/fanatic/repository/HybernateCustomerRepository.java
+++ src/org/fanatic/repository/HybernateCustomerRepository.java
@@ -14,9 +14,9 @@ public class HybernateCustomerRepository implements customerRepository {
 		List<Customer> customers = new ArrayList <Customer>();
 		Customer c = new Customer();
 		Customer c1 = new Customer();
-		c.setFirstName("Vivek");
-		c.setLastName("Kumar");
-		c1.setFirstName("Love");
+		c.setFirstName("Arun");
+		c.setLastName("Pratap");
+		c1.setFirstName("Prateek");
 		c1.setLastName("Kumar");
 		customers.add(c);
 		customers.add(c1);
